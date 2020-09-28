b4eb7f4ec890f2de15d4366db21230aa456a6724
mavrik83 <rjhuber83@gmail.com>
2020-09-27 20:38:08 -0400

    adds sessions_controller and login/logout methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 # frozen_string_literal: true
 
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      # flash[:success] = 'Logged in successfully'
+      redirect_to user
+    else
+      # flash.now[:danger] = 'The email/password combination you entered is incorrect.'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    # flash[:success] = "Log out successful"
+    redirect_to root_path
+  end
+end
