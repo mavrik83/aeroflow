f8b9de1f9722be87188caaa2f3b074f01a494978
mavrik83 <rjhuber83@gmail.com>
2020-09-27 21:52:12 -0400

    adds nested route for answers

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,17 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  resources :answers
+  # resources :answers
   resources :tags
   resources :categories
-  resources :questions
+  resources :questions do
+    resources :answers, shallow: true
+    # resources :articles do
+    #   resources :comments, only: [:index, :new, :create]
+    # end
+    # resources :comments, only: [:show, :edit, :update, :destroy]
+  end
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
