a4e754f8adf4c7c2b7436fb54c6baaadfa5bfa97
mavrik83 <rjhuber83@gmail.com>
2020-09-27 21:52:54 -0400

    adds validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   has_many :questions
   has_many :answers
   has_many :categories, through: :questions
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: {minimum: 3, maximum: 25}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    uniqueness: {case_sensitive: false},
+                    length: {maximum: 105},
+                    format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
