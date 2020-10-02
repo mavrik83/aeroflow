cbd40b16033bdec0f02afca5bd87d725ed3becab
mavrik83 <rjhuber83@gmail.com>
2020-10-02 15:19:26 -0400

    Update answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
-  has_and_belongs_to_many :tags
 
   validates :title, presence: true, length: { minimum: 6, maximum: 1000 }
   validates :content, presence: true, length: { minimum: 20, maximum: 3500 }
