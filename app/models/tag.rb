7632b2f5018d8abd7e820e276a87fb425a52d57d
mavrik83 <rjhuber83@gmail.com>
2020-10-02 15:20:13 -0400

    Delete tag.rb

--- app/models/tag.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Tag < ApplicationRecord
-  has_and_belongs_to_many :answers
-  has_and_belongs_to_many :questions
-  validates :name, presence: true, length: [within: 3..15]
-end
