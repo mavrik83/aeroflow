ea582af0e4f9f016a0036a2860209a82202ccd40
mavrik83 <rjhuber83@gmail.com>
2020-10-02 15:20:44 -0400

    adds destroy dependencies to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :questions
-  has_many :answers
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
   has_many :categories, through: :questions
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
-                       length: { minimum: 3, maximum: 25}
+                       length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true,
-                    uniqueness: {case_sensitive: false},
+                    uniqueness: { case_sensitive: false},
                     length: {maximum: 105},
                     format: {with: VALID_EMAIL_REGEX}
   has_secure_password
